ec7ba03045d640217d3334f773112de16ef83c26
Alek <alek.jakimovski1@gmail.com>
2021-07-08 12:56:53 +0200

    created karte component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,45 @@
 <template>
-  <h1>Hello</h1>
+  <div class="container">
+    <h1>Memory-Spiel</h1>
+    <section class="spielbrett">
+      <Karte
+        v-for="(karte, idx) in karteList"
+        :key="`karte-${idx}`"
+        :value="karte"
+      />
+    </section>
+  </div>
 </template>
 
 <script>
+import Karte from "./components/Karte.vue";
+
 export default {
   name: "App",
+  components: {
+    Karte,
+  },
+  setup() {
+    const karteList = [];
+
+    for (let i = 0; i < 16; i++) {
+      karteList.push(i);
+    }
+
+    return {
+      karteList,
+    };
+  },
 };
 </script>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,5 +47,29 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+}
+
+.karte {
+  border: 3px solid rgb(116, 116, 116);
+  border-radius: 10px;
+}
+
+.spielbrett {
+  margin-top: 50px;
+  display: grid;
+  grid-template-columns: 90px 90px 90px 90px;
+  grid-template-rows: 90px 90px 90px 90px;
+  grid-column-gap: 30px;
+  grid-row-gap: 30px;
+  justify-content: center;
 }
 </style>
---
+++ src/components/Karte.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="karte">{{ value }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
